59fd002c49c4921558d8084910c48f8c0239d1b3
nejeong <o_ink@naver.com>
2020-09-06 19:10:52 +0900

    JPA post insert

---
+++ src/main/java/com/hj/webservice/springboot/web/PostsApiController.java
@@ -0,0 +1,24 @@
+package com.hj.webservice.springboot.web;
+
+import com.hj.webservice.springboot.service.PostsService;
+import com.hj.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.hj.webservice.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+}
